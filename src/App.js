2e813d9eb0cb5c72be76bf55b0f9cbb77ad92ead
shashankrk46 <shashankrk46@gmail.com>
2020-01-26 17:55:26 +0530

    added searchbox

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {robots} from './robots';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+
+
+
+const App=()=>{
+    return (
+        <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox/>
+        <CardList robots={robots}/>
+        </div>
+    )
+}
+
+export default App ;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
+import App from './App'
 
-import {robots} from './robots';
-import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-<CardList robots={robots}/>
+<App/>
 
 , document.getElementById('root'));
 
